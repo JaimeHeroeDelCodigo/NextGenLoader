1f684ab954e26bf4f6cd41a8e3f986d2617dfe24
JaimeHeroeDelCodigo <jaime_magoazul@live.com.mx>
2025-11-29 22:53:23 -0600

    Title corrected.

--- src/main/java/org/example/nextgenloader/HelloApplication.java
+++ src/main/java/org/example/nextgenloader/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException, InterruptedException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("NextGenLoader");
         stage.setScene(scene);
         stage.show();
 
