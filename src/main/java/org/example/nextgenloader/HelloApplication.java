79bfdb3a24a84c3e30aef947f132ff7325304390
JaimeHeroeDelCodigo <jaime_magoazul@live.com.mx>
2025-11-30 18:16:26 -0600

    Method to open the file explorer renamed to openFileExplorerClick.

--- src/main/java/org/example/nextgenloader/HelloApplication.java
+++ src/main/java/org/example/nextgenloader/HelloApplication.java
@@ -3,7 +3,9 @@ package org.example.nextgenloader;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -11,7 +13,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 420, 440);
+
+
+
+        Scene scene = new Scene(fxmlLoader.load(), 470, 440);
         stage.setTitle("NextGenLoader");
         stage.setScene(scene);
         stage.show();
--- src/main/java/org/example/nextgenloader/HelloController.java
+++ src/main/java/org/example/nextgenloader/HelloController.java
@@ -2,13 +2,21 @@ package org.example.nextgenloader;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void openFileExplorerClick()() {
+
+
+
+        FileChooser fileChooser = new FileChooser();
         welcomeText.setText("New loading started");
+        
+
     }
 }
