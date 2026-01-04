4af2bf8417b31f5c2b59fd0c7e2d806465a9b1b3
JaimeHeroeDelCodigo <jaime_magoazul@live.com.mx>
2026-01-03 22:23:51 -0600

    Files and configuration added for packaging the application into a jar

---
+++ src/main/java/org/example/nextgenloader/Launcher.java
@@ -0,0 +1,7 @@
+package org.example.nextgenloader;
+
+public class Launcher {
+    public static void main(String[] args) throws InterruptedException {
+        NextGenLoaderApplication.main(args);
+    }
+}
